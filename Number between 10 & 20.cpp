3706477fa47fe34b37914e4caafd4b1a7bdd0a67
UndeShravani <shravani.unde24@sanjivani.edu.in>
2024-09-23 22:53:55 +0530

    Add files via upload

---
+++ Number between 10 & 20.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+ cout<<"Enter a number:";
+ cin>>num;
+ if(num>=10 && num<=20){
+   cout<<"The number is between 10 and 20"<<endl;
+ }else
+ {
+   cout<<"The number is not between 10 and 20"<<endl;
+ }
+ return 0;
+ 
+}
