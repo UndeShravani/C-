67076fc2495eebe662419f3d21bdd0cf6c93b01f
UndeShravani <shravani.unde24@sanjivani.edu.in>
2024-09-23 22:51:57 +0530

    Add files via upload

---
+++ Float number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float a=3.14;
+  double b=3.1415926535;
+  long double c=3.1465353865488567768696L;
+  cout<<"Float value:"<<a<<endl;
+  cout<<"Double value:"<<b<<endl;
+  cout<<"Long double value:"<<c<<endl;
+  return 0;
+}
