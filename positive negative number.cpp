c9dc280265b94f24389faa4c7547bbbdd5814931
UndeShravani <shravani.unde24@sanjivani.edu.in>
2024-09-23 22:53:15 +0530

    Add files via upload

---
+++ positive negative number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int number ;
+cout<<"Enter a number :";
+cin>>number;
+if(number>=0){
+  cout<<"The entered number is positive."<<endl;
+}else{
+  cout<<"The entered number is negative."<<endl;
+}
+return 0;
+  }
