83f882106929cab2c94aca43fa3d75523ce06ca8
UndeShravani <shravani.unde24@sanjivani.edu.in>
2024-10-08 23:43:09 +0530

    Add files via upload

---
+++ Round program.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+  int num=4;
+  int exp=3;
+  
+  cout<<"Rounding 5.6 gives:"<< round(5.6)<<endl;
+  cout<<"Rounding 5.4 gives:"<< round(5.4)<<endl;
+  return 0;
+}
