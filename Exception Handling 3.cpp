b3389f52d61b42551e96bee8a806dd9ef423a09c
UndeShravani <shravani.unde24@sanjivani.edu.in>
2024-11-07 22:06:03 +0530

    Add files via upload

---
+++ Exception Handling 3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int age;
+ cout<<"Enter yuor age";
+  cin>>age;
+  try{
+    try{
+      if(age<0){
+        throw"Age cannot be negative !";
+      }
+      cout<<"Your age is:"<<age<<endl;
+    }catch(const char* e){
+      cout<<"Caught in inner block:"<<e<<endl;
+      throw;}
+    }catch(const char* e){
+      cout<<"Caught in outer block:"<<e<<endl;
+    }
+  return 0;
+}
